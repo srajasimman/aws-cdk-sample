f4d57b83ebad412cd8b7ca6df1dfcbe79d44385d
Rajasimman S <srajasimman@gmail.com>
2025-08-12 17:58:12 +0530

    :tada: Initial Commit

---
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -0,0 +1,33 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
+import { HitCounter } from "./hitcounter";
+import { TableViewer } from "cdk-dynamo-table-viewer";
+
+export class CdkWorkshopStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // defines an AWS Lambda resource
+    const hello = new Function(this, "HelloHandler", {
+      runtime: Runtime.NODEJS_22_X, // execution environment
+      code: Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: "hello.handler", // file is "hello", function is "handler"
+    });
+
+    const helloWithCounter = new HitCounter(this, "HelloHitCounter", {
+      downstream: hello,
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    const gateway = new LambdaRestApi(this, "Endpoint", {
+      handler: helloWithCounter.handler,
+    });
+
+    const tv = new TableViewer(this, "ViewHitCounter", {
+      title: "Hello Hits",
+      table: helloWithCounter.table,
+    });
+  }
+}
